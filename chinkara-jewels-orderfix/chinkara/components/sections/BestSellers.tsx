738ad0d0e15f2b75ff245345c09b934ee5f5a95a
Sushntt <sushant072008@gmail.com>
2026-07-26 00:48:27 +0530

    Delete chinkara-jewels-orderfix/chinkara directory

--- chinkara-jewels-orderfix/chinkara/components/sections/BestSellers.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllProducts } from "@/lib/data"
-import { ProductCarousel } from "./ProductCarousel"
-
-export async function BestSellers() {
-  const products = await getAllProducts()
-  const bestsellers = products.filter((p) => p.bestseller)
-  const list = (bestsellers.length > 0 ? bestsellers : products).slice(0, 10)
-  return <ProductCarousel title="Best Sellers" products={list} viewAllHref="/shop?sort=bestseller" />
-}
